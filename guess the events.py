374dd8f9a60cd395f7c26671cdff0e63be099016
Kainatfatima711 <kainatfetima@gmail.com>
2026-05-30 12:47:47 +0500

    Add files via upload

---
+++ guess the events.py
@@ -0,0 +1,13 @@
+print("Types of Events")
+
+print("\n1. Tossing a coin and rolling a dice")
+print("Answer: Independent Event")
+
+print("\n2. Drawing two balls without replacement")
+print("Answer: Dependent Event")
+
+print("\n3. Getting heads and tails at the same time")
+print("Answer: Mutually Exclusive Event")
+
+print("\n4. A student liking cricket and football")
+print("Answer: Not Mutually Exclusive Event")
