4c6f1ba93585d0495f78bac1a23eaefd633f8520
Kainatfatima711 <kainatfetima@gmail.com>
2026-05-30 12:41:55 +0500

    Add files via upload

---
+++ winning chances.py
@@ -0,0 +1,12 @@
+red = 1
+blue = 6
+white = 3
+
+total_balls = red + blue + white
+
+p_first_white = white / total_balls
+
+p_second_white = white / total_balls
+
+print("Probability that the second ball is white given that the first ball is white:")
+print(p_second_white)
